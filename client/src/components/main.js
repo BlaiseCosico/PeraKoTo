7f1ff132c6eff849405a6de39326aa00b4c4d055
BlaiseCosico <Blaisecosico101@gmail.com>
2020-06-14 01:00:22 +0800

    added income panel

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -4,18 +4,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import RecentTransactions from "./RecentTransactions/RecentTransactions.js"
 import "../App.css"
 
-import process from "./transactions.js"
+import income from "./income.js"
 
 
 function MainBody(){
 	const col3Style = {
 		'background-color': 'red',
-		height: '300px'
+		height: '600px'
 	};
 
 	const col9Style = {
 		'background-color': 'green',
-		height: '300px'
+		height: '600px'
 	}
 	return(
 	<Router>
@@ -26,7 +26,10 @@ function MainBody(){
 				</div>
 
 				<div class="col-sm-9" style = {col9Style}>
-				<h4><small>Recent Transactions</small></h4>
+					<h4><small>Income</small></h4>
+					<Route path="/" exact component={income} />
+
+					<h4><small>Recent Transactions</small></h4>
 					<Route path="/" exact component={RecentTransactions} />
 				</div>
 			</div>
